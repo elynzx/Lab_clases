51bc78121625f5452ec814216d88f249f22926f1
Elynzx <rpascual06@outlook.com>
2026-09-25 17:27:49 -0500

    task 5

--- task5.js
+++ task5.js
@@ -1,7 +1,24 @@
 export class Jugador {
-  constructor() {
-
+  constructor(nombre, nivel) {
+    this.nombre = nombre;
+    this.nivel = nivel;
+    this.experiencia = 0;
+    this.expNivel = 100;
+  }
+  informacion() {
+    return `${this.nombre} ha alcanzado el Nivel ${this.nivel}!`;
+  }
+  ganarExperiencia(puntos) {
+    this.experiencia = this.experiencia + puntos;
+    console.log(`${this.nombre} has ganado ${puntos} de experiencia!`);
+    this.subirNivel();
+    console.log(this.informacion());
   }
 
-
+  subirNivel() {
+    if (this.experiencia >= this.expNivel) {
+      this.experiencia = this.experiencia - this.expNivel;
+      this.nivel++;
+    }
+  }
 }
